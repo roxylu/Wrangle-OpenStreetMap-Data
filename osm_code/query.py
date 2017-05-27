92f7c3267846c43d6c7025b9d0fe9bc993d84026
roxylu <roxylu816@gmail.com>
2017-05-27 10:39:14 +0800

    Overview of the data

--- osm_code/query.py
+++ osm_code/query.py
@@ -8,7 +8,7 @@ def get_db(db_name):
     return db
 
 
-def make_pipeline():
+def make_pipeline_for_unique_users():
     pipeline = [
         {'$match': {'type': {'$exists': 1}}},
         {'$group': {'_id': '$created.user',
@@ -18,20 +18,61 @@ def make_pipeline():
     return pipeline
 
 
+def make_pipeline_for_yearly_contribution():
+    pipeline = [
+        {'$project': {
+            'contribute_year': {'$substr': ['$created.timestamp', 0, 4]}
+        }},
+        {'$group': {'_id': '$contribute_year',
+                    'count': {'$sum': 1}}},
+        {'$sort': {'_id': 1}}
+    ]
+    return pipeline
+
+
+def make_pipeline_for_shops():
+    pipeline = [
+        {'$match': {'shop': {'$exists': 1}}},
+        {'$group': {'_id': '$shop',
+                    'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}},
+        {'$limit': 5}
+    ]
+    return pipeline
+
+
+def make_pipeline_for_supermarket():
+    pipeline = [
+        {'$match': {'shop': 'supermarket',
+                    'name': {'$exists': 1}}},
+        {'$group': {'_id': '$name',
+                    'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}},
+        {'$limit': 5}
+    ]
+    return pipeline
+
+
 def aggregate(db, pipeline):
     return [doc for doc in db.shanghai.aggregate(pipeline)]
 
 
-def process_result(db, result):
+if __name__ == "__main__":
+    db = get_db('osm')
+
+    pipeline = make_pipeline_for_unique_users()
+    result = aggregate(db, pipeline)
     # Number of contributing users
     print "Number of contributing users: "
     total_users = len(result)
     print total_users
+    print "==========================================="
 
     # Top contributing user as a percentage of total documents
     print "Top contributing user as a percentage of total documents:"
     top_user = result[0]
     print "{0:.2f} [%]".format(top_user['count'] * 100.0 / db.shanghai.count())
+    print "==========================================="
 
     # Top 10% of contributing user as a percentage of total documents
     print "Top 10% of contributing user as a percentage of total documents:"
@@ -39,10 +80,28 @@ def process_result(db, result):
     for i in range(int(total_users * 0.1)):
         sum += result[i]['count']
     print "{0:.2f} [%]".format(sum * 100.0 / db.shanghai.count())
+    print "==========================================="
 
-
-if __name__ == "__main__":
-    db = get_db('osm')
-    pipeline = make_pipeline()
+    pipeline = make_pipeline_for_yearly_contribution()
     result = aggregate(db, pipeline)
-    process_result(db, result)
+    # Contributions by year
+    print "Contributions by year:"
+    print "Year: Contributions"
+    for year in result:
+        print year['_id'] + ": " + str(year['count'])
+    print "==========================================="
+
+    # Number of top 5 shops
+    print "Number of top 5 shops:"
+    pipeline = make_pipeline_for_shops()
+    result = aggregate(db, pipeline)
+    for shop in result:
+        print shop['_id'] + ": " + str(shop['count'])
+    print "==========================================="
+
+    # Number of top 5 supermarket
+    print "Number of top 5 supermarket:"
+    pipeline = make_pipeline_for_supermarket()
+    result = aggregate(db, pipeline)
+    for shop in result:
+        print shop['_id'] + ": " + str(shop['count'])
